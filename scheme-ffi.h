824bc88616d87e8758a83a0963394c69d2cd9d06
Andrés Musetti Millán <andresmusetti@gmail.com>
2012-03-02 17:00:15 -0200

    Added some FFI helper macros.
    - hastag(fptr,tag)
    
    Ask wether a foreign_ptr* has the given tag.
    It may have a NULL pointer.
    
    - hasptr(fptr,tag)
    
    Ask wether a foriegn_ptr* has the given tag, and its pointer is not NULL.
    
    - getptr(fptr)
    
    Get a foreign_ptr*'s pointer value.
    
    - clrptr(fptr)
    
    Clear the foreign_ptr* completely. This will set the foreign_ptr* fields
    pointer, tag, and finalizer, to their respective null values.
    The object will not call a finalizer when finished, and any resource pointed to
    solely by its pointer will be lost.
    
    - internfinalizer(symbol,finptr)
    
    Internalize an immutable symbol with a finalizer that will be called upon exit.
    The finalizer has the following type:
        typedef void (*ffi_finalizer)(scheme*);

--- scheme-ffi.h
+++ scheme-ffi.h
@@ -4,26 +4,34 @@
 #include "scheme.h"
 #include "scheme-private.h"
 
-#define dbgmsg(msg)					\
-  do {							\
-    fprintf(stderr,__FILE__ ":%d: " msg, __LINE__);	\
-    fflush(stderr);					\
+#include <stdio.h>
+#define dbgmsg(msg)                                    \
+  do {                                                 \
+    fprintf(stderr,__FILE__ ":%d: " msg, __LINE__);    \
+    fflush(stderr);                                    \
   } while(0)
 
-#define getarg(a,type,conv)					\
-  do{								\
-    pointer p;							\
-    if(args == sc->NIL){					\
-      dbgmsg("Missing argument of type "#type"!\n");		\
-      return sc->F;						\
-    }								\
-    p = pair_car(args);						\
-    args = pair_cdr(args);					\
-    if(!is_##type(p)){						\
-      dbgmsg("Argument should be of type "#type"!\n");		\
-      return sc->F;						\
-    }								\
-    (a) = conv(p);						\
+/* Convenience macros to manage (foreign_ptr*) */
+#define hastag(fp,tag) ((fp)->_tag == (tag))
+#define hasptr(fp,tag) ((fp)->_ptr && hastag(fp,tag))
+#define getptr(fp) ((fp)->_ptr)
+#define clrptr(fp) do{(fp)->_ptr=NULL;(fp)->_fin=NULL;(fp)->_tag=0;}while(0)
+
+/* Convenience macros to handle foreign function arguments. */
+#define getarg(a,type,conv)                                     \
+  do{                                                           \
+    pointer p;                                                  \
+    if(args == sc->NIL){                                        \
+      dbgmsg("Missing argument of type "#type"!\n");            \
+      return sc->F;                                             \
+    }                                                           \
+    p = pair_car(args);                                         \
+    args = pair_cdr(args);                                      \
+    if(!is_##type(p)){                                          \
+      dbgmsg("Argument should be of type "#type"!\n");          \
+      return sc->F;                                             \
+    }                                                           \
+    (a) = conv(p);                                              \
   }while(0)
 
 #define getnarg(a)                                              \
@@ -52,27 +60,48 @@
 #define parg(a) getarg(a,foreignptr,fpvalue)
 #define symarg(a) getarg(a,symbol,symname)
 
-#define internfun(symbol,fptr)			\
-  scheme_define(sc				\
-		,sc->global_env			\
-		,mk_symbol(sc,symbol)		\
-		,mk_foreign_func(sc,fptr))
+/* Convenience macros to internalize objects into Scheme. */
 
-#define internval(symbol,pval)			\
-  scheme_define(sc				\
-		,sc->global_env			\
-		,mk_symbol(sc,symbol)		\
-		,pval)
+#define internfun(symbol,fptr)                  \
+  scheme_define(sc                              \
+                ,sc->global_env                 \
+                ,mk_symbol(sc,symbol)           \
+                ,mk_foreign_func(sc,fptr))
 
-#define internconst(symbol,pval)		\
-  do{						\
-    pointer s = mk_symbol(sc,symbol);		\
-    pointer v = pval;				\
-    setimmutable(s);				\
-    setimmutable(v);				\
-    scheme_define(sc,sc->global_env,s,v);	\
+#define internval(symbol,pval)                  \
+  scheme_define(sc                              \
+                ,sc->global_env                 \
+                ,mk_symbol(sc,symbol)           \
+                ,pval)
+
+#define internconst(symbol,pval)                \
+  do{                                           \
+    pointer s = mk_symbol(sc,symbol);           \
+    pointer v = pval;                           \
+    setimmutable(s);                            \
+    setimmutable(v);                            \
+    scheme_define(sc,sc->global_env,s,v);       \
   }while(0)
 
+
+/* FFI finalizer function. */
+typedef void(*ffi_finalizer)(scheme*sc);
+
+#define FFI_FINALIZER_TAG 0xD1EFF10B
+#define internfinalizer(symbol,finptr)          \
+  do{                                           \
+    foreign_ptr fp;                             \
+    fp._ptr = sc;                               \
+    fp._fin = (finalizer)finptr;                \
+    fp._tag = FFI_FINALIZER_TAG;                \
+    internconst(symbol,mk_foreign_ptr(sc,&fp)); \
+  }while(0)
+
+/* Convenience macros to define foreign functions. */
 #define defun(name) pointer name(scheme* sc, pointer args)
+
+/* Convenience functions to manage vectors. */
+#define newvec(size) sc->vptr->mk_vector(sc,(size))
+#define setvec(v,i,e) sc->vptr->set_vector_elem((v),(i),(e))
 
 #endif/* ! SCHEME_FFI_HEADER */
