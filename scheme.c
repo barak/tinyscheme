a0188fe1e14a5a692354215c3ef882e98cd6f9fd
kcozens <kcozens@4a0124f5-7288-4c88-8df5-efb20b5748ad>
2020-06-04 16:14:00 +0000

    Finally applied a patch from 2004 that quiets some compiler warnings.
    git-svn-id: svn://svn.code.sf.net/p/tinyscheme/code/trunk@118 4a0124f5-7288-4c88-8df5-efb20b5748ad

--- scheme.c
+++ scheme.c
@@ -3676,6 +3676,7 @@ static pointer opexe_3(scheme *sc, enum scheme_opcodes op) {
                case OP_GRE:   comp_func=num_gt; break;
                case OP_LEQ:   comp_func=num_le; break;
                case OP_GEQ:   comp_func=num_ge; break;
+               default:       break;    /* Quiet the compiler */
           }
           x=sc->args;
           v=nvalue(car(x));
@@ -3919,7 +3920,8 @@ static pointer opexe_4(scheme *sc, enum scheme_opcodes op) {
           switch(op) {
                case OP_OPEN_INFILE:     prop=port_input; break;
                case OP_OPEN_OUTFILE:    prop=port_output; break;
-               case OP_OPEN_INOUTFILE: prop=port_input|port_output; break;
+               case OP_OPEN_INOUTFILE:  prop=port_input|port_output; break;
+               default:                 break;  /* Quiet the compiler */
           }
           p=port_from_filename(sc,strvalue(car(sc->args)),prop);
           if(p==sc->NIL) {
@@ -3936,6 +3938,7 @@ static pointer opexe_4(scheme *sc, enum scheme_opcodes op) {
           switch(op) {
                case OP_OPEN_INSTRING:     prop=port_input; break;
                case OP_OPEN_INOUTSTRING:  prop=port_input|port_output; break;
+               default:                   break;    /* Quiet the compiler */
           }
           p=port_from_string(sc, strvalue(car(sc->args)),
                  strvalue(car(sc->args))+strlength(car(sc->args)), prop);
